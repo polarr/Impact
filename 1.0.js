520814fbfa8717068658da3af2fb05dfd984b9f6
1egend <51487023+1e9end@users.noreply.github.com>
2020-10-06 17:55:45 -0400

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -1,15 +1,14 @@
 var gl;
-var initgl = function(canvas){
+function initGL(canvas){
     var c = document.getElementById(canvas);
     if (!c || c.tagName !== "CANVAS"){
-        throw new Error("Invalid canvas id or id tag");
+        throw new Error("Invalid canvas ID or ID tag");
     }
     var gl = c.getContext("webgl", { preserveDrawingBuffer: true, antialias: false, premultipliedAlpha: false });
 
     if (!gl){
-        throw new Error("WebGL not supported, try better browser");
+        throw new Error("Your browser does not support WebGL.");
     }
-
     return gl;
 }
 
@@ -17,9 +16,7 @@ var createShader = function(type, source){
     var shader = gl.createShader(type);
     gl.shaderSource(shader, document.getElementById(source).text);
     gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);  
-
-    if (success){
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
         console.log("Successfully created shader of type " + (type === gl.VERTEX_SHADER ? "vertex shader" : "fragment shader"));
         return shader;
     }
@@ -37,9 +34,8 @@ var createProgram = function(vts, fts){
     gl.attachShader(program, createShader(gl.FRAGMENT_SHADER, fts));
     console.log("Successfully attached fragment shader with source "  + gl.getShaderSource(createShader(gl.FRAGMENT_SHADER, fts)));
     gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
     
-    if (success){
+    if (gl.getProgramParameter(program, gl.LINK_STATUS)){
         console.log("Sucessfully created program");
         return program;
     }
@@ -49,19 +45,18 @@ var createProgram = function(vts, fts){
     gl.deleteProgram(program);
 }
 
-// Rendering 
-function resize(width, height, square) {
-    var w = width || window.innerWidth;
-    var h = height || window.innerHeight;
-    var d = Math.min(w, h);
-    if (square){
-        canvas.width = d;
-        canvas.height = d;
-        gl.viewport(0, 0, d, d);
-    }
-    else{
-        canvas.width = w;
-        canvas.height = h;
-        gl.viewport(0, 0, w, h);
-    }
+function resize(canvas, w, h, sq) {
+    var W  = w || window.innerWidth;  
+    var H = h || window.innerHeight;
+
+    var SQ = Math.min(W, H);
+    canvas.width = sq ? SQ:W;
+    canvas.height = sq ? SQ:H;
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 };
+
+function clearColor(){
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
